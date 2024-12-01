afbae6ef0e18c4446b480278279fe3f4458d9869
Jennifer0903 <u3589926@connect.hku.hk>
2024-12-02 00:14:19 +0800

    Update correctguess.cpp
    Updated correctguess.cpp with 1 line added for the hint function

--- correctguess.cpp
+++ correctguess.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 \\ use namespace std? depends on main code
 
-std::string CorrectGuess(const std::string& targetWord, const std::string& guess) {
+std::string CorrectGuess(const std::string& targetWord, const std::string& guess, std::vector<bool>& found) {
     std::string feedback;
 
     for (size_t i = 0; i < targetWord.length(); ++i) {
@@ -12,6 +12,7 @@ std::string CorrectGuess(const std::string& targetWord, const std::string& guess
 
         if (guessChar == targetChar) {
             feedback += "<span style='color: green'>" + std::string(1, guessChar) + "</span>";
+            found[i] =true; 
         } else if (targetWord.find(guessChar) != std::string::npos) {
             feedback += "<span style='color: yellow'>" + std::string(1, guessChar) + "</span>";
         } else {
