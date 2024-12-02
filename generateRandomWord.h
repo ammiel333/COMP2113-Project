511bb07d6015670a1a35f9c27cf72f94d0bb8ebb
dkyng1125 <ydkii26@connect.hku.hk>
2024-12-02 17:26:26 +0800

    Update generateRandomWord.h

--- generateRandomWord.h
+++ generateRandomWord.h
@@ -1,8 +1,19 @@
-#ifndef GENERATERANDOMWORD_H
-#define GENERATERANDOMWORD_H
+#ifndef GENERATE_RANDOM_WORD_H
+#define GENERATE_RANDOM_WORD_H
 
 #include <string>
+#include <random>
 
-string generateRandomWord(const string& fileName);
+struct WordList {
+  std::string* words;
+  int size;
+
+};
+
+WordList loadWords(const std::string& fileName);
+
+void freeWordList(WordList& wordList);
+
+std::string generateRandomWord(WordList& wordList);
 
 #endif
