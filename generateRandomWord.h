273ea0feb0a5fe63bf9cedc93008772dfa479b10
dkyng1125 <ydkii26@connect.hku.hk>
2024-12-05 01:23:28 +0800

    Update generateRandomWord.h

--- generateRandomWord.h
+++ generateRandomWord.h
@@ -17,3 +17,18 @@ void freeWordList(WordList& wordList);
 std::string generateRandomWord(WordList& wordList);
 
 #endif
+
+/* How to call the function: 
+
+loadWords(fileName)
+Specify the file name that you want to load the words from 
+It will load the words in that specified file then store in dynamically allocated allay 
+
+freeWordList(wordList)
+Parameter wordList is a dnamically allocated array that store the words for Wordle game
+This function can be called to free the allocated memory for wordList 
+
+generateRandomWord(wordList)
+Paramter is wordList
+By using wordList, this function return the randomly generated word which will be the answer of the game
+*/
