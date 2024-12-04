32a0a5fe53af539c09a7786e33d7650b656200e5
codeoflassitude <kshahid@connect.hku.hk>
2024-12-04 13:53:36 +0800

    Create correctguess.h

---
+++ correctguess.h
@@ -0,0 +1,9 @@
+#ifndef CORRECTGUESS_H
+#define CORRECTGUESS_H
+
+#include <string>
+#include <vector>
+
+std::string CorrectGuess(const std::string& targetWord, const std::string& guess, std::vector<bool>& found, bool useColors);
+
+#endif
