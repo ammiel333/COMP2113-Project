8fcefb91fabbd1e70031bfcce2d14c31117858b8
codeoflassitude <kshahid@connect.hku.hk>
2024-12-04 14:05:50 +0800

    Update correctguess.cpp

--- correctguess.cpp
+++ correctguess.cpp
@@ -1,44 +1,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+\\ use namespace std? depends on main code
 
-std::string CorrectGuess(const std::string& targetWord, const std::string& guess, std::vector<bool>& found, bool useColors) {
-    std::string feedback; // declaring the feedback string to return
-    std::vector<bool> used(targetWord.length(), false); // tracks which positions in the target word are "used" or "taken"
+std::string CorrectGuess(const std::string& targetWord, const std::string& guess, std::vector<bool>& found) {
+    std::string feedback;
 
-    // check for letters in the correct position (will be green)
     for (size_t i = 0; i < targetWord.length(); ++i) {
-        if (guess[i] == targetWord[i]) {
-            if (useColors) {
-                feedback += "\033[32m"; // green colour for correct position
-                feedback += guess[i];
-                feedback += "\033[0m";  // reset the colour
-            } else {
-                feedback += "[G]"; // indicate the correct position without colour
-                feedback += guess[i];
-            }
-            found[i] = true; // mark as found in the correct position
-            used[i] = true;  // mark this position in the target word as used/taken
+        char guessChar = guess[i];
+        char targetChar = targetWord[i];
+
+        if (guessChar == targetChar) {
+            feedback += "<span style='color: green'>" + std::string(1, guessChar) + "</span>";
+            found[i] =true; 
+        } else if (targetWord.find(guessChar) != std::string::npos) {
+            feedback += "<span style='color: yellow'>" + std::string(1, guessChar) + "</span>";
         } else {
-            feedback += "_"; // temporary placeholder for now
+            feedback += "<span style='color: grey'>" + std::string(1, guessChar) + "</span>";
         }
     }
 
-    // check for letters in the wrong position (will be yellow)
-    for (size_t i = 0; i < targetWord.length(); ++i) {
-        if (feedback[i] == '_') { // if not already marked as correct
-            bool isFound = false;
-
-            // to check if the letter exists elsewhere in the target word because there can be more than use of the same letter in a word
-            for (size_t j = 0; j < targetWord.length(); ++j) {
-                if (!used[j] && guess[i] == targetWord[j]) {
-                    isFound = true;
-                    used[j] = true; // mark this position in the target word as used/taken
-                    break;
-                }
-            }
-
-            if (isFound) {
-                if (useColors) {
-                    feedback[i] = '\033'; // yellow colouring (wrong position but right letter)
-                    feedback.append("\033");
+    return feedback;
+}
