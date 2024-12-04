7b771daa3ae8d02ef510cec8eda80851f5af9843
Jennifer0903 <u3589926@connect.hku.hk>
2024-12-05 00:53:03 +0800

    Update gamemessage.h

--- gamemessage.h
+++ gamemessage.h
@@ -1,26 +1,12 @@
 #ifndef GAMEMESSAGE_H
 #define GAMEMESSAGE_H
 #include <string>
-#define RESET "\033[0m"
-#define BLACK "\033[30m"              /* Black */
-#define RED "\033[31m"                /* Red */
-#define GREEN "\033[32m"              /* Green */
-#define YELLOW "\033[33m"             /* Yellow */
-#define BLUE "\033[34m"               /* Blue */
-#define MAGENTA "\033[35m"            /* Magenta */
-#define CYAN "\033[36m"               /* Cyan */
-#define WHITE "\033[37m"              /* White */
 
-#define B_BLACK   "\033[40m"    /* Black */
-#define B_RED     "\033[41m"    /* Red */
-#define B_GREEN   "\033[42m"    /* Green */
-#define B_YELLOW  "\033[43m"    /* Yellow */
-#define B_BLUE    "\033[44m"    /* Blue */
-#define B_MAGENTA "\033[45m"    /* Magenta */
-#define B_CYAN    "\033[46m"    /* Cyan */
-#define B_WHITE   "\033[47m"    /* White */
-#define B_GRAY "\033[100m"      /* Gray */
-
+constexpr const char* BLACK = "\033[30m"; /* Black */
+constexpr const char* B_GREEN = "\033[42m"; /* Green */
+constexpr const char* B_YELLOW = "\033[43m"; /* Yellow */
+constexpr const char* B_GRAY = "\033[100m"; /* Gray */
+constexpr const char* RESET = "\033[0m"; /* Reset */
 
 void gameInstructions();
 
